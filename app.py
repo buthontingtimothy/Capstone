930e9feb0777cf17a08835370d5e87d6af2d6879
buthontingtimothy <buthontingtimothy@gmail.com>
2025-02-20 06:47:31 +0800

    Update app.py
    1) Correct Typos
    2) Add Comments
    3) Add dict for filter
    4) Add Path for embedding matrix and user data
    5) Add error message if no recommendation
    6) Fix __path__ error

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+# Description: This is the main file for the Streamlit app. It contains the user interface and the logic to call the recommendation function.
+
+# Import necessary libraries
+# data manipulation
 import pandas as pd
+# streamlit
 import streamlit as st
+# function to generate top matches from input.py
 from input import generate_top_matches_result
+# image
 from PIL import Image
+# torch
+import torch
+
 
 # Streamlit app
 def main():
+    # prevent __path__ error
+    torch.classes.__path__ = []
+    
+    # Set image and the title
     image = Image.open("pic.png")
     st.image(image)
     st.title('Find Your Perfect Match on Earth')
@@ -66,7 +80,7 @@ def main():
         if education_option == '<select>':    
             education_option = 'N/A'
 
-        #ethnicity(muiti)
+        #ethnicity(multi)
         choice_ethnicity = ['asian', 'white', 'black', 'indian', 'hispanic / latin', 'other', 'native american', 'pacific islander', 'middle eastern']
         ethnicity_option = ", ".join(st.multiselect('Ethnicity (you can select more than one):', choice_ethnicity))
 
@@ -89,16 +103,16 @@ def main():
         if want_kids_option == '<select>':    
             want_kids_option = 'N/A'
 
-        #pets_have(muiti)
+        #pets_have(multi)
         choice_pets_have = ['has dogs', 'has cats', 'no pets']
         have_pets_option = ", ".join(st.multiselect('Do you have any dogs or cats? (you can select more than one):', choice_pets_have, max_selections=2))
 
-        if 'no pets' in have_pets_option and any(option in have_pets_option for option in ['has dogs', 'has cats']):
+        if 'no pets' in have_pets_option and ('has dogs' in have_pets_option or 'has cats' in have_pets_option):
             have_pets_option = 'N/A'
             st.warning("You cannot select any other options when 'no pets' is chosen.")
 
 
-        #pets_like(muiti)
+        #pets_like(multi)
         choice_pets_like = ['likes dogs', 'likes cats', 'neutral to pets', 'dislikes dogs', 'dislikes cats']
         like_pets_option = ", ".join(st.multiselect('What is your preference regarding dogs and cats? (you can select more than one):', choice_pets_like, max_selections=2))
         
@@ -109,18 +123,19 @@ def main():
             like_pets_option = 'N/A'
             st.warning("You cannot select 'likes cats' and 'dislikes cats' at the same time.")
         if 'neutral to pets' in like_pets_option and any(option in like_pets_option for option in ['likes dogs', 'likes cats', 'dislikes dogs', 'dislikes cats']):
+            like_pets_option = 'N/A'            
             st.warning("You cannot select 'neutral to pets' along with any other preference.")
-            like_pets_option = 'N/A'
+
 
         #religion
-        choice_religin = ['<select>', 'Agnosticism', 'Atheism', 'Buddhism', 'Catholicism', 'Christianity','Hinduism', 'Islam', 'Judaism', 'Other', 'Irreligion']
-        choice_religin_atti = ['very serious about it', 'somewhat serious about it', 'not too serious about it', 'laughing about it']
-        religion_option = st.selectbox('What is your religion? ', choice_religin)
+        choice_religion = ['<select>', 'Agnosticism', 'Atheism', 'Buddhism', 'Catholicism', 'Christianity','Hinduism', 'Islam', 'Judaism', 'Other', 'Irreligion']
+        choice_religion_atti = ['very serious about it', 'somewhat serious about it', 'not too serious about it', 'laughing about it']
+        religion_option = st.selectbox('What is your religion? ', choice_religion)
         if religion_option == '<select>':    
             religion_option = 'N/A'
         if religion_option not in ["Agnosticism", "Atheism", "Irreligion"]:
             # Attitude to religion
-            atti_religion_option = st.select_slider('How seriously do you take your religion? (if any):', options = choice_religin_atti)
+            atti_religion_option = st.select_slider('How seriously do you take your religion? (if any):', options = choice_religion_atti)
         else:
             atti_religion_option = 'N/A'
 
@@ -136,32 +151,31 @@ def main():
         if smoke_option == '<select>':    
             smoke_option = 'N/A'
 
-        #speaks_language(muiti)
+        #speaks_language(multi)
         choice_language = ['afrikaans', 'albanian', 'ancient', 'arabic', 'armenian', 'basque', 'belarusan', 'bengali', 'breton', 'bulgarian', 'catalan', 'cebuano', 'chechen', 'chinese', 'croatian', 'c++', 'czech', 'danish', 'dutch', 'english', 'esperanto', 'estonian', 'farsi', 'finnish', 'french', 'frisian', 'georgian', 'german', 'greek', 'gujarati', 'hawaiian', 'hebrew', 'hindi', 'hungarian', 'icelandic', 'ilongo', 'indonesian', 'irish', 'italian', 'japanese', 'khmer', 'korean', 'latin', 'latvian', 'lisp', 'lithuanian', 'malay', 'maori', 'mongolian', 'norwegian', 'occitan', 'persian', 'polish', 'portuguese', 'romanian', 'rotuman',  'russian', 'sanskrit', 'sardinian', 'serbian', 'sign', 'slovak', 'slovenian', 'spanish', 'swahili', 'swedish', 'tagalog', 'tamil',  'thai', 'tibetan', 'turkish', 'ukrainian', 'urdu', 'vietnamese', 'welsh', 'yiddish', 'other']
         language_option = ", ".join(st.multiselect('What language(s) do you speak?', choice_language))
 
         #essay
-        essay_input = st.text_input("Tell me a little more about yourself", value=" ", max_chars= 200)
+        essay_input = st.text_input("Tell me a little more about yourself", value="", max_chars= 200)
         if essay_input:
             st.write(f"{essay_input}")
-
+    
+    # Filters
     with col2:
         st.subheader("Filters")
-
+        # Filter by age, height, language, ethnicity, religion, sign, and keyword
         age_range = st.slider("Age Range:", min_value=18, max_value=100, value=(18, 35))
         height_range = st.slider("Height Range (cm):", min_value=100, max_value=250, value=(150, 200))
         # location = st.text_input("Preferred Location (city, state, or country):")
         speaks = st.multiselect("Preferred Languages:", choice_language)
         ethnicity = st.multiselect("Preferred Ethnicity:", choice_ethnicity)
-        religion = st.multiselect("Preferred Religion:", choice_religin)
+        religion = st.multiselect("Preferred Religion:", choice_religion)
         sign = st.multiselect("Preferred Astrological Sign:", choice_sign)
         keyword_filter = st.text_input("Keyword Filter (e.g., hobbies, interests):")
 
     st.subheader('- End of Data Input -')
 
     st.divider()
-
-
 
     #Summary
     st.subheader("Summary of your info:")
@@ -175,7 +189,7 @@ def main():
     st.write('Your drinking habit: ', drinks_option)
     st.write('Drug use: ', drugs_option)
     st.write('Your education level: ', education_option)
-    st.write('Your ethicity: ', ethnicity_option)
+    st.write('Your ethnicity: ', ethnicity_option)
     st.write('Your job / industry: ', job_option)
     st.write('Do you have kids? ', have_kids_option)
     st.write('Do you want (more) kids? ', want_kids_option)
@@ -187,10 +201,12 @@ def main():
     st.write('Your zodiac sign: ', zodiac_option)
     st.write('Do you smoke? ', smoke_option)
     st.write('The language(s) you speak : ', language_option)
+    st.write('Tell me a little more about yourself: ', essay_input)
 
 
-
-    input_dict = {'age': age_input,
+    # Create a dictionary to store the user input
+    input_dict = {
+        'age': age_input,
         'status': status_option,
         'sex': sex_option,
         'orientation': orientation_option,
@@ -203,13 +219,26 @@ def main():
         'height': height_input,
         'job': job_option,
         'location': pd.NA,
-        'offspring': have_kids_option + ', '+ want_kids_option,
-        'pets': have_pets_option + ', ' + like_pets_option,
+        'offspring': ', '.join(filter(lambda x: x != 'N/A', [have_kids_option, want_kids_option])),
+        'pets': ', '.join(filter(lambda x: x != 'N/A', [have_pets_option, like_pets_option])),
         'religion': religion_option,
         'sign': zodiac_option,
         'smokes': smoke_option,
         'speaks': language_option,
-        'essay_all': "about me"}
+        'essay_all': essay_input
+        }
+    
+    # Create a dictionary to store the filters
+    filter_dict = {
+        'age_range': age_range,
+        'height_range': height_range,
+        # 'location': location,
+        'speaks': speaks,
+        'ethnicity': ethnicity,
+        'religion': religion,
+        'sign': sign,
+        'keyword_filter': keyword_filter
+        }
 
 
 # Get recommendations when the user clicks the button
@@ -218,16 +247,17 @@ def main():
        # Call the recommendation function
         recommendations = generate_top_matches_result(
             input_dict,
-            age_range=age_range,
-            height_range=height_range,
-            # location=location,
-            speaks=speaks,
-            ethnicity=ethnicity,
-            religion=religion,
-            sign=sign,
-            # keyword_filter=keyword_filter
+            top_n=10,
+            path_embeddings_matrix='okcupid_profiles_preprocessed.npy',
+            path_user_data="okcupid_profiles_cleaned.csv",
+            **filter_dict
         )
-        st.dataframe(recommendations, width=1000)
+        # check if recommendations is not None and display the recommendations
+        if recommendations is not None:
+            st.dataframe(recommendations, width=1000)
+        else:
+            st.error("No recommendations found. Please try again with different inputs.")
 
+# Run the app only if this script is run directly (not imported)
 if __name__ == "__main__":
     main()
